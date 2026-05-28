37efaeee7db2a328e5cf0aa1cbb9c83e9f098d81
erikagiella <erikagiella93@gmail.com>
2026-05-28 14:01:47 +0200

    Stilizzazione

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,94 @@
 import { useState } from 'react'
 
-
 function App() {
 
   const articlesList = [
-    "Il cambiamento climantico",
-    "La nuova Ferrari elettrica",
-    "Gatti neri: portano davvero sfortuna?"
+    "Il cambiamento climatico esiste?",
+    "La nuova Ferrari elettrica: innovazione o scempio?",
+    "Gatti neri: portano davvero sfortuna?",
+    "I Watussi erano davvero alti?",
+    "Non sei pronto per la prova costume? Vai in montagna!"
   ]
 
-  const [articles, setArticles] = useState(articlesList);
-  const [newArticle, setNewArticle] = useState('');
+  const [articles, setArticles] = useState(articlesList)
+  const [newArticle, setNewArticle] = useState('')
 
   function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
+
+    if (!newArticle.trim()) return
+
     setArticles([newArticle, ...articles])
-    setNewArticle('');
+    setNewArticle('')
   }
 
-  function deleteArticle(i) {
-    const updatedArticles = articles.filter((article, index) => {
-      return index !== i;
-    }
+  function deleteArticle(indexToRemove) {
+    const updatedArticles = articles.filter(
+      (_, index) => index !== indexToRemove
     )
-    setArticles(updatedArticles);
+
+    setArticles(updatedArticles)
   }
 
   return (
     <>
-      <header className="container text-center my-3">
-        <h1>
-          <i className="bi bi-bookshelf mx-3"></i>
+      <header className="container text-center py-4">
+        <h1 className="fw-bold">
+          <i className="bi bi-bookshelf me-3"></i>
           React Blog Form
         </h1>
       </header>
-      <main className='container'>
-        <form onSubmit={handleSubmit}>
-          <input type='text' placeholder='Add a new title' value={newArticle} onChange={e => setNewArticle(e.target.value)} />
-          <button>Add article</button>
-        </form>
 
-        {articles.length === 0 ?
-          (<div className='text-center fs-3'> No more articles left!</div>)
-          :
-          (<ul className="list-group my-3">
-            {articles.map((article, i) =>
-              <li className='list-group-item d-flex justify-content-between' key={`${i}-article`}>
-                {article}
-                <button className='btn btn-danger' style={{ cursor: 'pointer' }}
-                  onClick={() => deleteArticle(i)}>
-                  <i className="bi bi-trash-fill"></i>
-                </button>
-              </li>
-            )}
-          </ul>)}
+      <main className="container">
 
+        <div className="card shadow-sm border p-3">
+          <form
+            className="input-group mb-4"
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Add a new article..."
+              value={newArticle}
+              onChange={(e) => setNewArticle(e.target.value)}
+            />
+
+            <button className="btn btn-primary fw-bolder">
+              <i className="bi bi-plus-lg me-2"></i>
+              Add
+            </button>
+          </form>
+
+
+          {articles.length === 0 ? (
+            <div className="text-center py-4">
+              <h4 className="text-muted">
+                No more articles left!
+              </h4>
+            </div>
+          ) : (
+            <ul className="list-group">
+              {articles.map((article, i) => (
+                <li
+                  key={`${i}-article`}
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                >
+                  <span>{article}</span>
+
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => deleteArticle(i)}
+                  >
+                    <i className="bi bi-trash-fill"></i>
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+
+
+        </div>
       </main >
     </>
   )
