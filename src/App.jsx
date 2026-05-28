188cb8e68434590a286ff699202d9b54ec5e74ba
erikagiella <erikagiella93@gmail.com>
2026-05-28 12:46:34 +0200

    input legato a newArticle + handleSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ function App() {
   ]
 
   const [articles, setArticles] = useState(articlesList);
+  const [newArticle, setNewArticle] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
 
   return (
     <>
@@ -20,8 +26,8 @@ function App() {
         </h1>
       </header>
       <main className='container'>
-        <form >
-          <input type='text' placeholder='Add a new title' />
+        <form onSubmit={handleSubmit}>
+          <input type='text' placeholder='Add a new title' value={newArticle} onChange={e => setNewArticle(e.target.value)} />
           <button>Add article</button>
         </form>
         <ul className="list-group my-3">
