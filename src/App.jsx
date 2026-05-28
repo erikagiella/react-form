4cf30841581a9e0e9572ef8218d47c9a22a7addb
erikagiella <erikagiella93@gmail.com>
2026-05-28 11:51:58 +0200

    struttura main

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 function App() {
 
+
   return (
     <>
       <header className="container text-center my-3">
@@ -11,7 +12,15 @@ function App() {
           React Blog Form
         </h1>
       </header>
-      <main></main>
+      <main className='container'>
+        <form>
+          <input type='text' placeholder='Add a new title' />
+          <button type='submit'>Add article</button>
+        </form>
+        <div className="card my-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit autem laborum debitis commodi vitae, eligendi rem, ad blanditiis magnam natus enim tempore dignissimos consequuntur! Facere nostrum suscipit quos possimus dignissimos.
+        </div>
+      </main>
     </>
   )
 }
