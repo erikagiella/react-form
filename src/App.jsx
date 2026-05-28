64390fa6d10af84a02a94269c6ab925329b58689
erikagiella <erikagiella93@gmail.com>
2026-05-28 11:45:23 +0200

    bootstrap + header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ function App() {
 
   return (
     <>
-
+      <header className="container text-center my-3">
+        <h1>
+          <i className="bi bi-bookshelf mx-3"></i>
+          React Blog Form
+        </h1>
+      </header>
+      <main></main>
     </>
   )
 }
