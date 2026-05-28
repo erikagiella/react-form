76e3529ab81e6999b3482a6a35218859ab267a9f
erikagiella <erikagiella93@gmail.com>
2026-05-28 14:08:05 +0200

    Componente AppHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import AppHeader from './assets/components/AppHeader'
 
 function App() {
 
@@ -32,12 +33,7 @@ function App() {
 
   return (
     <>
-      <header className="container text-center py-4">
-        <h1 className="fw-bold">
-          <i className="bi bi-bookshelf me-3"></i>
-          React Blog Form
-        </h1>
-      </header>
+      <AppHeader />
 
       <main className="container">
 
---
+++ src/assets/components/AppHeader.jsx
@@ -0,0 +1,10 @@
+export default function AppHeader() {
+    return (
+        <header className="container text-center py-4">
+            <h1 className="fw-bold">
+                <i className="bi bi-bookshelf me-3"></i>
+                React Blog Form
+            </h1>
+        </header>
+    )
+}
